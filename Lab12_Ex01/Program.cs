134b7f0b76430819ec9f5bd0375f572877b047a5
juneroku <horizondawn2547@gmail.com>
2025-03-29 17:32:50 +0700

    Finish homework

---
+++ Lab12_Ex01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Person
+{
+    private string name = string.Empty;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
+
+class Student : Person
+{
+    // Student class inherits from Person, so it can use Name property directly.
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student1 = new Student();
+        student1.Name = "Somchai";
+        Console.WriteLine($"student1 name = {student1.Name}");
+
+        var student2 = new Student();
+        student2.Name = "Sompong";
+        Console.WriteLine($"student2 name = {student2.Name}");
+    }
+}
---
+++ Lab12_Ex05/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+interface IBaseClass1
+{
+    void Method1();
+}
+
+interface IBaseClass2
+{
+    void Method2();
+}
+
+class BaseClass1 : IBaseClass1
+{
+    public BaseClass1()
+    {
+        Console.WriteLine("This is BaseClass1");
+    }
+
+    public void Method1()
+    {
+        Console.WriteLine("Method1 from BaseClass1");
+    }
+}
+
+class BaseClass2 : IBaseClass2
+{
+    public BaseClass2()
+    {
+        Console.WriteLine("This is BaseClass2");
+    }
+
+    public void Method2()
+    {
+        Console.WriteLine("Method2 from BaseClass2");
+    }
+}
+
+class DerivedClass : BaseClass1, IBaseClass2
+{
+    public DerivedClass() : base()
+    {
+        Console.WriteLine("This is DerivedClass");
+    }
+
+    // กำหนดการใช้งาน Method2 ของ IBaseClass2 ที่ DerivedClass ไม่ได้สืบทอดโดยตรง
+    public void Method2()
+    {
+        Console.WriteLine("Method2 from DerivedClass");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DerivedClass dc = new DerivedClass();  // การสร้างออบเจกต์
+        dc.Method1();  // เรียก Method1 จาก BaseClass1
+        dc.Method2();  // เรียก Method2 จาก DerivedClass
+    }
+}
